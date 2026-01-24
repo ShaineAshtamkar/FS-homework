acba76bd2cf7d84e5339bad1bf23552436772f48
Shaine <shaineashtamkar099@gmail.com>
2026-01-24 15:18:30 +0200

    added addWord function

--- AutoCompleteTrieProject/index.js
+++ AutoCompleteTrieProject/index.js
@@ -4,6 +4,7 @@ class AutoCompleteTrie {
         this.children = {};
         this.endOfWord = false;
     }
+
     addWord(word) {
         word = word.toLowerCase();
         let current = this;
@@ -15,41 +16,4 @@ class AutoCompleteTrie {
         }
         current.endOfWord = true;
     }
-    findWord(word) {
-        word = word.toLowerCase();
-        let current = this;
-        for (let ch of word) {
-            if (!current.children[ch]) {
-                return false;
-            }
-            current = current.children[ch];
-        }
-        if (current.endOfWord) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-    _getRemainingTree(prefix, node) {
-        prefix = prefix.toLowerCase();
-        let current = node;
-        for (let ch of prefix) {
-            if (!current.children[ch]) {
-                return null;  // prefix not found
-            }
-            current = current.children[ch];
-        }
-        return current; // node where prefix ends
-    }
-
-    _allWordsHelper(prefix, node, allWords) {
-
-    }
-    predictWords(prefix) {
-
-    }
-
 }
-
-
-module.exports = AutoCompleteTrie;
